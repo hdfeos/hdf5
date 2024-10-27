91e651ecf242011f0e8727ee4f29064caf625dad
hdfeos <hdfeos@users.noreply.github.com>
2024-10-27 03:35:41 +0000

    deploy: e76ffef959080fc7a5b8b0a43818187b4c88fe7a

--- develop/search/pages_4.js
+++ develop/search/pages_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['errors_20and_20exceptions_13159',['Errors and Exceptions',['../_e_r_r_o_r_s.html',1,'']]],
-  ['examples_20by_20api_13160',['Examples by API',['../_ex_a_p_i.html',1,'']]],
-  ['examples_20from_20learning_20the_20basics_13161',['Examples from Learning the Basics',['../_l_b_examples.html',1,'']]],
-  ['extendible_20datasets_13162',['Extendible Datasets',['../_l_b_ext_dset.html',1,'LearnBasics']]]
+  ['errors_20and_20exceptions_0',['Errors and Exceptions',['../_e_r_r_o_r_s.html',1,'']]],
+  ['examples_20by_20api_1',['Examples by API',['../_ex_a_p_i.html',1,'']]],
+  ['examples_20from_20learning_20the_20basics_2',['Examples from Learning the Basics',['../_l_b_examples.html',1,'']]],
+  ['extendible_20datasets_3',['Extendible Datasets',['../_l_b_ext_dset.html',1,'LearnBasics']]]
 ];
